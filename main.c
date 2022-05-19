33393bb17455c2c7e1643eb6a64e4bb2f041e941
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-18 19:36:03 -0700

    task 0

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @ac: argument counter
+ * @av: command arguments
+ *
+ * Return: 0
+ */
+int main(int ac, const char**av)
+{
+	int a = 0;
+
+	while (a < ac)
+	{
+		printf("argv[%d]: %s\n", a, av[a]);
+		a++;
+	}
+	printf("Betty is beautiful\n");
+	return (0);
+}
