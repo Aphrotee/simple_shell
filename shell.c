7717d6fdcd2ee297d37567dee74978a066de4c86
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-26 08:46:24 -0700

    shell task

--- shell.c
+++ shell.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stddef.h>
 
 /**
  * main - shell
- * @ac: argument counter
- * @av: argument array
  *
  * Return: Always 0.
  */
-int main(int ac, const char *av[])
+int main(void)
 {
 	size_t n = 0;
-	char *narg[] = {NULL, NULL};
+	char *narg[21], *tok, *cmd = (char *)malloc(sizeof(char) * 100);
+	char *a = (char *)malloc(sizeof(char) * 1024);
+	char *nenv[100] = {(char *)"PATH=/bin", 0};
+	int i = 0, j = 0;
 
-	printf("aphrotee@2106~$");
-	getline(&narg[0], &n, stdin);
-	execve(narg[0], narg, NULL);
-	main(ac, av);
-	return (0);
+	printf("aphrotee@2106~$ ");
+	getline(&a, &n, stdin);
+	tok = strtok(a, " \n");
+	while (tok != NULL)
+	{
+		narg[j] = strdup(tok);
+		tok = strtok(NULL, " \n");
+		j++;
+	}
+	narg[j] = NULL;
+	strcpy(cmd, narg[0]);
+	if (strcmp(cmd, "exit") == 0)
+	{
+		printf("!");
+		return (0);
+	}
+	if (fork() != 0)
+		wait(NULL);
+	else
+	{
+		i = execve(cmd, narg, nenv);
+		if (i == -1)
+		{
+			strcpy(cmd, "/bin/");
+			strcat(cmd, narg[0]);
+			strcpy(narg[0], cmd);
+			i = execve(cmd, narg, nenv);
+		}
+	}
+	main();
+	return(0);
 }
